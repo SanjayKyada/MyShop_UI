2dc2c32c603abf63b3c5873d91e1257761939a0d
Sanjay <sanjay.kyadaty@gmail.com>
2019-08-10 13:09:12 +0530

    All basic methods for product class.

---
+++ Data/Model/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class Product
+    {
+        public string PID { get; set; }
+        [StringLength(20)]
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(10,10000,ErrorMessage ="Please enter price between 10 to 10000.")]
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+
+        public Product()
+        {
+            PID = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,79 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cacheObj = MemoryCache.Default;
+        List<Product> products;
+
+        //basic initization for product list and cache.
+        public ProductRepository()
+        {
+            products = cacheObj["Products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        // Updating cache with object list.
+        public void Commit()
+        {
+            cacheObj["Products"] = products;
+        }
+
+        // to get single product
+        public Product GetProductDetail(string Id)
+        {
+            return IsProduct(Id);
+        }
+
+        //Add product in list/database.
+        public void InsertProduct(Product product)
+        {
+            products.Add(product);
+            Commit();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            Product oldProduct = IsProduct(product.PID);
+            oldProduct.Category = product.Category;
+            oldProduct.Description = product.Description;
+            oldProduct.Image = product.Image;
+            oldProduct.Name = product.Name;
+            oldProduct.Price = product.Price;
+            Commit();
+        }
+
+        //get all products.
+        public IQueryable<Product> GetAllProduct()
+        {
+            return products.AsQueryable();
+        }
+
+        //Delete Product.
+        public void DeleteProduct(string Id)
+        {
+            Product product = IsProduct(Id);
+            products.Remove(product);
+            Commit();
+        }
+
+        // for retrieving the product.
+        public Product IsProduct(string productID)
+        {
+            Product product = products.SingleOrDefault(x => x.PID == productID);
+            if (product != null)
+                return product;
+            else
+                throw new Exception("Product not found in list.");
+        }
+    }
+}
