8eb374afd50ac94f1f3d0280009940058ef17194
Sanjay <sanjay.kyadaty@gmail.com>
2019-08-16 19:34:24 +0530

    Done Listing products

--- MyShop_UI/Controllers/HomeController.cs
+++ MyShop_UI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Model;
+using Data.Model;
+using DataAccess.InMemory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,28 @@ namespace MyShop_UI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepositoryBase<Product> productRepository;
+        IRepositoryBase<Category> categoryRepository;
+
+        public HomeController(IRepositoryBase<Product> prjRepo, IRepositoryBase<Category> catRepo)
+        {
+            productRepository = prjRepo;
+            categoryRepository = catRepo;
+        }
+
+        //Listing all products in a closer look to products view listing....
         public ActionResult Index()
         {
-            return View();
+            return View(productRepository.GetAllData().ToList());
+        }
+        //get full detail of particular product.
+        public ActionResult Detail(string Id)
+        {
+            Product prj = productRepository.GetDetail(Id);
+            if (prj == null)
+                return HttpNotFound();
+            else
+                return View(prj);
         }
 
         public ActionResult About()
